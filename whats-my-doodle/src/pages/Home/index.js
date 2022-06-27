8cb71d32e25068a177a8d0d9a2848afb9e91e8e9
Amir <aha000111@icloud.com>
2022-06-27 14:07:38 +0100

    add canvas for drawing

--- whats-my-doodle/src/pages/Home/index.js
+++ whats-my-doodle/src/pages/Home/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import Canvas from '../../components/Canvas/Canvas';
 
 const Home = () => {
   return (
     <h1>
         Home Page
     </h1>
+    
   )
 }
 
-export default Home;
+function canvasApp() {
+  return (
+    <div className="canvasApp">
+      <Canvas
+        width={500}
+        height={300}
+      />
+    </div>
+  );
+}
+
+
+export default canvasApp;
