a16296368ea5235b1e4145501bdc1ea1b6e1edf8
Shoreen Beckford <shoreen.beckford@gmail.com>
2022-06-27 12:24:24 +0100

    Fix Routes

--- whats-my-doodle/src/App.js
+++ whats-my-doodle/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Home, NotFound } from "./pages";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
