0e58343d0e05ee039be2b8dd0be17ce462260fdf
Izgardon <williamsessions23@gmail.com>
2022-06-28 15:40:51 +0100

    working on new canvas

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,10 +1,22 @@
 import { useOnDraw } from "./Hooks";
+import { socket } from "../../App";
+import React, { useState } from "react";
 
 const Canvas = ({ width, height }) => {
+  const [room, setRoom] = useState("");
+  const [user, setUser] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  socket.on("recieveData", (roomData, userData, playersData) => {
+    setPlayers([...playersData]);
+    setRoom(roomData);
+    setUser(userData);
+  });
+
   const { setCanvasRef, onCanvasMouseDown } = useOnDraw(onDraw);
 
   function onDraw(ctx, point, prevPoint) {
-    drawLine(prevPoint, point, ctx, "#000000", 5);
+    drawLine(prevPoint, point, ctx, "red", 5);
   }
 
   function drawLine(start, end, ctx, color, width) {
@@ -22,11 +34,21 @@ const Canvas = ({ width, height }) => {
     ctx.fill();
   }
 
+  function allDraw() {
+    onCanvasMouseDown();
+    socket.emit("allDrawRequest", room);
+  }
+
+  socket.on("allDraw", () => {
+    console.log("this works");
+    onCanvasMouseDown();
+  });
+
   return (
     <canvas
       width={width}
       height={height}
-      onMouseDown={onCanvasMouseDown}
+      onMouseDown={allDraw}
       style={canvasStyle}
       ref={setCanvasRef}
     />
