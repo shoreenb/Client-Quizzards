cfebd281b21d6d10581e3dd936e541a4daad2699
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-28 14:48:11 +0100

    declare states in User component

--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { socket } from "../../App";
 
 export default function Users() {
+  const [room, setRoom] = useState("");
+  const [user, setUser] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  socket.on("recieveData", (room, user, players) => {
+    setRoom(room);
+    setUser(user);
+    setPlayers(players);
+  });
+  console.log(room, user, players);
   return (
     <>
       <h4>User Section</h4>
