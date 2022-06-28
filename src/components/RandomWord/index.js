bf037505e872e8f18b8bcd1f4b8dddfd33b64b66
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-28 17:05:58 +0100

    layout of game page

--- src/components/RandomWord/index.js
+++ src/components/RandomWord/index.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { socket } from "../../App";
 
 export default function RandomWord() {
+  const [category, setCatergory] = useState("");
+
   socket.on("recieveCatergory", (room, catergoryInput) => {
-    console.log(room, catergoryInput);
+    console.log("Hello from rC", catergoryInput);
+    setCatergory(catergoryInput);
   });
-  return (
-    <>
-      <h3>RandomWord</h3>
-    </>
-  );
+
+  return <>{category ? <h3>{category}</h3> : <h3>Error</h3>}</>;
 }
