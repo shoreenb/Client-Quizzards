485c41f766710d5f646e1a693a5c63e6c822dcde
Amir <aha000111@icloud.com>
2022-06-29 09:21:27 +0100

    add countdown component test

---
+++ src/components/Canvas/canvas.test.js
@@ -0,0 +1,12 @@
+// import Canvas from ".";
+// import { screen, render } from '@testing-library/react';
+// import { MemoryRouter } from 'react-router-dom';
+
+// describe('Canvas', () => {
+//     test('it renders Canvas', () => {
+//         render(<Canvas />, { wrapper: MemoryRouter });
+//         const drawing = screen.queryByRole('Canvas');
+//         expect(drawing).toBeInTheDocument();
+//     })
+    
+// })
