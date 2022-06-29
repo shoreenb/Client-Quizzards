5c042bc53d867ccce7f1e94e899a6c93b47313bf
Sam Hardiment <samuelghardiment@gmail.com>
2022-06-29 10:19:38 +0100

    add api call for catergory

--- src/components/RandomWord/index.js
+++ src/components/RandomWord/index.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 import { socket } from "../../App";
+import axios from "axios";
 
 export default function RandomWord() {
   const [category, setCatergory] = useState("");
+  const [error, setError] = useState("");
 
   socket.on("recieveCatergory", (room, catergoryInput) => {
     console.log("Hello from rC", catergoryInput);
     setCatergory(catergoryInput);
   });
 
+  const getWords = async (category) => {
+    try {
+      const data = await axios.get(
+        `https://quizzards-the-game.herokuapp.com/${category}`
+      );
+      console.log(data);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
   return (
     <div className="randomWordContainer">
       <div>
-        {category ? (
-          <h3 className="gameTitle">Catergory: {category}</h3>
+        {error ? (
+          <h3>{error}</h3>
         ) : (
-          <h3>Error</h3>
+          <h3 className="gameTitle">Catergory: {category}</h3>
         )}
       </div>
       <div>
